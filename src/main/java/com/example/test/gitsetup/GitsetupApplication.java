57973c3207538820378f02221b5e950664dca0a0
kavishamodi97 <kavishamodi971@gmail.com>
2021-02-12 12:44:35 +0530

    Initial Commit

---
+++ src/main/java/com/example/test/gitsetup/GitsetupApplication.java
@@ -0,0 +1,13 @@
+package com.example.test.gitsetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitsetupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitsetupApplication.class, args);
+    }
+
+}
